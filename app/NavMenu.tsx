cdb68c9823e6e81f00c99cb56a4323b4f9fafe96
Daria <dariastoicad@gmail.com>
2023-06-09 20:54:14 +0200

    navbar

---
+++ app/NavMenu.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function NavMenu() {
+  return (
+    <nav className="flex justify-between p-10">
+      <Link href="/">
+        <Image
+          src="Nextspace.svg"
+          alt=""
+          width={50}
+          height={50}
+          className="w-56"
+        ></Image>
+      </Link>
+      <ul className="flex gap-4">
+        <li>
+          <Link href="">About</Link>
+        </li>
+        <li>
+          <Link href="">Blog</Link>
+        </li>
+        <li>
+          <Link href="">Users</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
