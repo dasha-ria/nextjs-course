8bb81806b3b8b1c2b5dedb6777b71b9b10a33bde
Daria <dariastoicad@gmail.com>
2023-06-09 21:14:02 +0200

    metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from "next";
+
+export const dynamic = "force-static";
+
+export const metadata: Metadata = {
+  title: "About Us",
+  description: "We are a social media company",
+};
+
 export default function About() {
   return (
     <div className="pl-10">
